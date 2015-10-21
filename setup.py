1c848d2469b9837a16302afdaaa4bfe017f35a09
Gael Pasgrimaud <gael@gawel.org>
2015-10-21 19:08:26 +0200

    Back to development: 0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.4'
+version = '0.5.dev0'
 
 entry_point = 'gp.recipe.tox:Recipe'
 entry_points = {"zc.buildout": ["default = %s" % entry_point]}
